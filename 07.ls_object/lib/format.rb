7efb3c3bf2eacd72752aafc83ad4d19b4f15abd6
kurosu <kurosu.66@gmail.com>
2024-09-22 10:23:11 +0900

    contentsをインスタンス変数からメソッドに変更

--- 07.ls_object/lib/format.rb
+++ 07.ls_object/lib/format.rb
@@ -12,12 +12,11 @@ require 'debug' # todo 後で消す
 
 
 class Format
-  attr_reader :directory_contents, :params, :contents
+  attr_reader :directory_contents, :params
 
   def initialize(directory_contents, params)
     @params = params
     @directory_contents = directory_contents
-    @contents = directory_contents.contents #ここがエラー。 2024/09/21
   end
 
 
@@ -26,16 +25,16 @@ class Format
   end
 
   def format(directory_contents)
-    @contents << ' ' until (@contents.length % COLUMN_COUNT).zero?
+    contents << ' ' until (contents.length % COLUMN_COUNT).zero?
 
-    max_length = @contents.max_by(&:length).length
+    max_length = contents.max_by(&:length).length
     adjusted_max_length = max_length + FIXED_SPACE_SIZE
-    items_with_spaces = @contents.map do |v|
+    items_with_spaces = contents.map do |v|
       v + ' ' * (adjusted_max_length - v.length)
     end
 
     items = []
-    row = (@contents.length / COLUMN_COUNT).ceil
+    row = (contents.length / COLUMN_COUNT).ceil
     items_with_spaces.each_slice(row) do |c|
       items << c
     end
@@ -45,6 +44,11 @@ class Format
   def format_l_option(directory_contents)
 
   end
+
+  private
+
+  def contents = @directory_contents.contents
+
 end
 
 
