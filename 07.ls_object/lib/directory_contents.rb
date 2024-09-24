8a7ad50805a04e6b53b41a41d2dcbfe44f7b3328
kurosu <kurosu.66@gmail.com>
2024-09-24 23:33:49 +0900

    定数をクラス内部に移動

--- 07.ls_object/lib/directory_contents.rb
+++ 07.ls_object/lib/directory_contents.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-COLUMN_COUNT = 7
-FIXED_SPACE_SIZE = 4
-
-PERMISSION = {
-  0 => '---',
-  1 => '--x',
-  2 => '-w-',
-  3 => '-wx',
-  4 => 'r--',
-  5 => 'r-x',
-  6 => 'rx-',
-  7 => 'rwx'
-}.freeze
-
-FILE_TYPE = {
-  'fifo' => 'p',
-  'characterSpecial' => 'c',
-  'directory' => 'd',
-  'blockSpecial' => 'b',
-  'file' => '-',
-  'link' => 'l',
-  'socket' => 's'
-}.freeze
-
 class Directory_Contents
+  COLUMN_COUNT = 7
+  FIXED_SPACE_SIZE = 4
+
+  PERMISSION = {
+    0 => '---',
+    1 => '--x',
+    2 => '-w-',
+    3 => '-wx',
+    4 => 'r--',
+    5 => 'r-x',
+    6 => 'rx-',
+    7 => 'rwx'
+  }.freeze
+
+  FILE_TYPE = {
+    'fifo' => 'p',
+    'characterSpecial' => 'c',
+    'directory' => 'd',
+    'blockSpecial' => 'b',
+    'file' => '-',
+    'link' => 'l',
+    'socket' => 's'
+  }.freeze
+
   attr_reader :params, :contents
 
   def initialize(params)
