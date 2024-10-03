7dc2ec836ee2a01109eef7d9de84c1caf2b243de
kurosu <kurosu.66@gmail.com>
2024-10-03 23:18:43 +0900

    totalの出力漏れ修正

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -11,7 +11,7 @@ params = ARGV.getopts('a', 'r', 'l')
 directory_contents = DirectoryContents.new(params)
 lscommand = Format.new(directory_contents, params)
 if params['l']
-  puts lscommand.result['total_block']
+  puts "total #{lscommand.result['total_block']}"
   lscommand.result['files_l_option'].each { |v| puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}" }
 else
   lscommand.result.each { |v| puts v.join }
