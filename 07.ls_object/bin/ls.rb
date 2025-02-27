09b13f3960dca0f7e0e39b3cbbbaa5b77fb0bfa6
kurosu <kurosu.66@gmail.com>
2025-02-27 22:47:42 +0900

    rubocop修正

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -9,20 +9,19 @@ require_relative '../lib/format'
 require_relative '../lib/format_long_option'
 require_relative '../lib/command_line_option'
 
-
-command_line_option = CommandLineOption.new().params
+command_line_option = CommandLineOption.new.params
 directory = Directory.new(command_line_option)
 
 if command_line_option['l']
-  ls_command_long_option = FormatLongOption.new()
+  ls_command_long_option = FormatLongOption.new
   result = ls_command_long_option.result(directory)
 
-  puts "total #{ result[:total_block] }"
+  puts "total #{result[:total_block]}"
   result[:files_long_option].each do |v|
     puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
   end
 else
-  ls_command = Format.new()
+  ls_command = Format.new
   result = ls_command.result(directory)
   puts result.join
 end
--- 07.ls_object/lib/format_long_option.rb
+++ 07.ls_object/lib/format_long_option.rb
@@ -6,9 +6,7 @@ class FormatLongOption
 
     long_option_contents = {}
     long_option_contents[:total_block] = current_dir_items.sum { |v| v.fetch_lstat_blocks(v.file) }
-    long_option_contents[:files_long_option] = current_dir_items.map do |current_dir_item|
-      current_dir_item.fetch_lstat
-    end
+    long_option_contents[:files_long_option] = current_dir_items.map(&:fetch_lstat)
     long_option_contents
   end
 end
