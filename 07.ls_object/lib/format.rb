89bb014568e36309e69bdf0c74da915367be2ae3
kurosu <kurosu.66@gmail.com>
2025-04-08 19:49:58 +0900

    resultメソッドを分割

--- 07.ls_object/lib/format.rb
+++ 07.ls_object/lib/format.rb
@@ -6,13 +6,8 @@ class Format
 
   def result(directory)
     contents = directory.long_option_files.map(&:file)
-    contents << ' ' until (contents.length % COLUMN_COUNT).zero?
-
-    max_length = contents.max_by(&:length).length
-    adjusted_max_length = max_length + FIXED_SPACE_SIZE
-    items_with_spaces = contents.map do |v|
-      v + ' ' * (adjusted_max_length - v.length)
-    end
+    adjust_to_column_count(contents)
+    items_with_spaces = adjust_max_length(contents)
 
     items = []
     row = (contents.length / COLUMN_COUNT).ceil
@@ -20,5 +15,17 @@ class Format
       items << c
     end
     puts items.transpose.join
+  end
+
+  def adjust_to_column_count(contents)
+    contents << ' ' until (contents.length % COLUMN_COUNT).zero?
+  end
+
+  def adjust_max_length(contents)
+    max_length = contents.max_by(&:length).length
+    adjusted_max_length = max_length + FIXED_SPACE_SIZE
+    contents.map do |v|
+      v + ' ' * (adjusted_max_length - v.length)
+    end
   end
 end
