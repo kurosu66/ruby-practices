04b9bfd6dced647d234e66042732605f0592c100
kurosu <kurosu.66@gmail.com>
2025-04-21 19:31:33 +0900

    fommat_long_optionクラスのメソッド分割

--- 07.ls_object/lib/format_long_option.rb
+++ 07.ls_object/lib/format_long_option.rb
@@ -6,19 +6,25 @@ class FormatLongOption
   def result(directory)
     current_dir_items = directory.file_details
     long_option_contents = {}
-    build_long_option_contents(current_dir_items, long_option_contents)
-
-    puts "total #{long_option_contents[:total_block]}"
-    long_option_contents[:files_long_option].each do |v|
-      puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
-    end
+    long_option_contents = build_long_option_contents(current_dir_items)
+    output(long_option_contents)
   end
 
   private
 
-  def build_long_option_contents(current_dir_items, long_option_contents)
-    long_option_contents[:total_block] = current_dir_items.sum { |v| v.lstat_blocks }
-    long_option_contents[:files_long_option] = current_dir_items.map do |current_dir_item|
+  def build_long_option_contents(current_dir_items)
+    long_option_contents = {}
+    long_option_contents[:total_block] = calculate_total_blocks(current_dir_items)
+    long_option_contents[:files_long_option] = build_file_info_list(current_dir_items)
+    long_option_contents
+  end
+
+  def calculate_total_blocks(current_dir_items)
+    current_dir_items.sum(&:lstat_blocks)
+  end
+
+  def build_file_info_list(current_dir_items)
+    current_dir_items.map do |current_dir_item|
       lstat = current_dir_item.stat
       {
         permission: current_dir_item.permission,
@@ -29,6 +35,13 @@ class FormatLongOption
         time_stamp: current_dir_item.time_stamp,
         name: current_dir_item.file
       }
+    end
+  end
+
+  def output(long_option_contents)
+    puts "total #{long_option_contents[:total_block]}"
+    long_option_contents[:files_long_option].each do |v|
+      puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
     end
   end
 end
