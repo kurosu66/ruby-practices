52e522c7fc2778630840f7609b9b9cd7f0a33f63
kurosu <kurosu.66@gmail.com>
2025-05-03 19:22:57 +0900

    多態性を使うよう修正

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -11,10 +11,10 @@ require_relative '../lib/long_formatter'
 command_line_option = ARGV.getopts('a', 'r', 'l')
 directory = Directory.new(command_line_option)
 
+lscommand = nil
 if command_line_option['l']
-  ls_command_long_option = LongFormatter.new
-  ls_command_long_option.result(directory)
+  lscommand = LongFormatter.new
 else
-  ls_command = ShortFormatter.new
-  ls_command.result(directory)
+  lscommand = ShortFormatter.new
 end
+lscommand.result(directory)
