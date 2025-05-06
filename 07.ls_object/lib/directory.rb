5697e92dbcf53f9047ff290f3afaf3a48b11d8b7
kurosu <kurosu.66@gmail.com>
2025-05-06 09:54:20 +0900

    メソッド名修正

--- 07.ls_object/lib/directory.rb
+++ 07.ls_object/lib/directory.rb
@@ -6,12 +6,12 @@ class Directory
   attr_reader :file_details
 
   def initialize(command_line_option)
-    @file_details = fetch_file_info(command_line_option)
+    @file_details = fetch_file_details(command_line_option)
   end
 
   private
 
-  def fetch_file_info(command_line_option)
+  def fetch_file_details(command_line_option)
     files = command_line_option['a'] ? Dir.entries('.').sort : Dir.glob('*')
     files = files.reverse if command_line_option['r']
     files.map { |file| FileDetail.new(file) }
