5628c2da5a2a73d50f46ad74a1c5f9ca422ff6ff
kurosu <kurosu.66@gmail.com>
2025-05-08 19:22:42 +0900

    long_formatter側もcontentsからfilesに名称変更

--- 07.ls_object/lib/long_formatter.rb
+++ 07.ls_object/lib/long_formatter.rb
@@ -5,17 +5,17 @@ require_relative '../lib/file_detail'
 class LongFormatter 
   def result(directory)
     current_dir_items = directory.file_details
-    long_option_contents = build_long_option_contents(current_dir_items)
-    output(long_option_contents)
+    long_option_files = build_long_option_files(current_dir_items)
+    output(long_option_files)
   end
 
   private
 
-  def build_long_option_contents(current_dir_items)
-    long_option_contents = {}
-    long_option_contents[:total_block] = FileDetail.calculate_total_blocks(current_dir_items)
-    long_option_contents[:files_long_option] = build_file_info_list(current_dir_items)
-    long_option_contents
+  def build_long_option_files(current_dir_items)
+    long_option_files = {}
+    long_option_files[:total_block] = FileDetail.calculate_total_blocks(current_dir_items)
+    long_option_files[:files_long_option] = build_file_info_list(current_dir_items)
+    long_option_files
   end
 
   def build_file_info_list(current_dir_items)
@@ -32,9 +32,9 @@ class LongFormatter
     end
   end
 
-  def output(long_option_contents)
-    puts "total #{long_option_contents[:total_block]}"
-    long_option_contents[:files_long_option].each do |v|
+  def output(long_option_files)
+    puts "total #{long_option_files[:total_block]}"
+    long_option_files[:files_long_option].each do |v|
       puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
     end
   end
