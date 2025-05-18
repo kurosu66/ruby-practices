fc6d4c519baf7540a08db013df8c43b48f8cb56a
kurosu <kurosu.66@gmail.com>
2025-05-18 23:05:32 +0900

    hashに全ての情報をまとめて出力メソッドに渡していた部分の修正

--- 07.ls_object/lib/long_formatter.rb
+++ 07.ls_object/lib/long_formatter.rb
@@ -5,20 +5,18 @@ require_relative '../lib/file_detail'
 class LongFormatter 
   def result(directory)
     current_dir_items = directory.file_details
-    long_option_files = build_long_option_files(current_dir_items)
-    output(long_option_files)
+    total_block_size =  Directory.calculate_total_blocks(current_dir_items)
+    detailed_files = build_detailed_files(current_dir_items)
+    output(total_block_size, detailed_files)
   end
 
   private
 
-  def build_long_option_files(current_dir_items)
-    long_option_files = {}
-    long_option_files[:total_block] = Directory.calculate_total_blocks(current_dir_items)
-    long_option_files[:files_long_option] = build_file_info_list(current_dir_items)
-    long_option_files
+  def fetch_detailed_files(current_dir_items)
+    puts current_dir_items
   end
 
-  def build_file_info_list(current_dir_items)
+  def build_detailed_files(current_dir_items)
     current_dir_items.map do |current_dir_item|
       {
         permission: current_dir_item.permission,
@@ -32,9 +30,9 @@ class LongFormatter
     end
   end
 
-  def output(long_option_files)
-    puts "total #{long_option_files[:total_block]}"
-    long_option_files[:files_long_option].each do |v|
+  def output(total_block_size, detailed_files)
+    puts "total #{total_block_size}"
+    detailed_files.each do |v|
       puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
     end
   end
