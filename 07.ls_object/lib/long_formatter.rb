f8fac95cc32baf1683d1ab8ff472a614f5cda6d5
kurosu <kurosu.66@gmail.com>
2025-05-28 19:24:38 +0900

    build_detailed_filesを削除

--- 07.ls_object/lib/long_formatter.rb
+++ 07.ls_object/lib/long_formatter.rb
@@ -13,30 +13,15 @@ class LongFormatter
   def result
     current_dir_items = @directory.file_details
     total_block_size =  @directory.calculate_total_blocks(current_dir_items)
-    detailed_files = build_detailed_files(current_dir_items)
-    output(total_block_size, detailed_files)
+    output(total_block_size, current_dir_items)
   end
 
   private
 
-  def build_detailed_files(current_dir_items)
-    current_dir_items.map do |current_dir_item|
-      {
-        permission: current_dir_item.permission,
-        n_link: current_dir_item.nlink.to_s.rjust(FIXED_SPACE_SIZE),
-        owner: current_dir_item.owner,
-        group: current_dir_item.group,
-        size: current_dir_item.size.to_s.rjust(FIXED_SPACE_SIZE),
-        time_stamp: current_dir_item.time_stamp,
-        name: current_dir_item.file_name
-      }
-    end
-  end
-
-  def output(total_block_size, detailed_files)
+  def output(total_block_size, current_dir_items)
     puts "total #{total_block_size}"
-    detailed_files.each do |v|
-      puts "#{v[:permission]} #{v[:n_link]} #{v[:owner]} #{v[:group]} #{v[:size]} #{v[:time_stamp]} #{v[:name]}"
+    current_dir_items.each do |v|
+      puts "#{v.permission} #{v.nlink.to_s.rjust(FIXED_SPACE_SIZE)} #{v.owner} #{v.group} #{v.size.to_s.rjust(FIXED_SPACE_SIZE)} #{v.time_stamp} #{v.file_name}"
     end
   end
 end
