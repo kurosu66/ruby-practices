ba54fde1011c17b19926ae39bdc69749d9b65b89
kurosu <kurosu.66@gmail.com>
2025-05-31 13:41:25 +0900

    current_dir_itemsをinitializeに移動

--- 07.ls_object/lib/long_formatter.rb
+++ 07.ls_object/lib/long_formatter.rb
@@ -8,19 +8,19 @@ class LongFormatter
 
   def initialize(directory)
     @directory = directory
+    @current_dir_items = @directory.file_details
   end
 
   def result
-    current_dir_items = @directory.file_details
-    total_block_size =  @directory.calculate_total_blocks(current_dir_items)
-    output(total_block_size, current_dir_items)
+    total_block_size =  @directory.calculate_total_blocks(@current_dir_items)
+    output(total_block_size)
   end
 
   private
 
-  def output(total_block_size, current_dir_items)
+  def output(total_block_size)
     puts "total #{total_block_size}"
-    current_dir_items.each do |v|
+    @current_dir_items.each do |v|
       puts "#{v.permission} #{v.nlink.to_s.rjust(FIXED_SPACE_SIZE)} #{v.owner} #{v.group} #{v.size.to_s.rjust(FIXED_SPACE_SIZE)} #{v.time_stamp} #{v.file_name}"
     end
   end
